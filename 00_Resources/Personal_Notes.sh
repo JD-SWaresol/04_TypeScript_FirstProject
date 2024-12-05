14664e168f4315dc341936257fd53fde9e7ebe19
JD-SWaresol <juan.delgado@smartwaresol.com>
2024-12-05 11:31:28 -0600

    WIP_04: Configuring the TS Compiler

--- 00_Resources/Personal_Notes.sh
+++ 00_Resources/Personal_Notes.sh
@@ -14,7 +14,16 @@
 
     tsc 'filename'.ts
 
-# 
+
+# Create a configuration file called 'ts.config.json' with some settings
+
+    tsc --init
+
+
+# Create a configuration in our project using 'tsconfig.json'
+
+    tsc
+
 
 
 ///// NOTES //////
@@ -26,3 +35,24 @@
     2.- Redeclaring a variable in the same block.
 
     let age: number = 20; <- 'age' is a TypeScript global world
+
+
+# ~/tsconfig.json
+
+    This files has a number of settings and the most of these are commented out by default. We are only going to 
+    use a handful of them. 
+    The commands have instructions.
+
+    Some Instructions are:
+        
+        target : es2016         # Specifies the version of JS that the TS compiler is going to generate. The 2016 is implemented in all browsers out there
+
+        module : commonjs       # This specifies the directory that contains our source files
+
+        rootDir : ./            # The './' represents the current folder. And we set './src'
+
+        outDir : ./             # Specifies the directory that will contain our JS files. And we set './dist'
+
+        removeComments : true   # Is this is enabled the TS compiler is going to remove all commnets that we add in our TS code, so the generated JS code is going to be shorter 
+
+        noEmitOnError: true     # By default whe we compile our code even if you have errors in out code TS compiler will still generate JS files. If exists any error, the TS compiler is not going to geenrate any JS files.
